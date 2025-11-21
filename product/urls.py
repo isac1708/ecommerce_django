f68580a7637bd9e5fcc9df79eb5c586c6b327372
isac1708 <isac.figueredo@gmail.com>
2025-11-21 18:00:42 -0300

    Feat: Added ProductDetail and CategoryDetail endpoints

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,11 @@ from product import views
 #rota para acessar essa view de produtos mais recentes
 urlpatterns = [
     path('latest-products/', views.LatestProductsList.as_view()),
+    # Nova rota para buscar detalhes do produto
+    # Ex: /api/v1/products/eletronicos/iphone-15/
+    path('products/<slug:category_slug>/<slug:product_slug>/', views.ProductDetail.as_view()),
+
+    # Nova rota para buscar categoria
+    # Ex: /api/v1/products/eletronicos/
+    path('products/<slug:category_slug>/', views.CategoryDetail.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -1,13 +1,38 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 #busca todos os produtos mais recentes no banco e passa pelo serializer.
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, CategorySerializer
 
 class LatestProductsList(APIView):
     def get(self, request, format=None):
         # Pega os primeiros 4 produtos (fatiamento de lista [0:4])
         products = Product.objects.all()[0:4]
         serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class ProductDetail(APIView):#Recebe dois slugs (categoria e produto) para garantir URL única e busca o item exato.
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+class CategoryDetail(APIView):#Busca a categoria e passa pelo serializer, ele já vai trazer a lista de produtos daquela categoria automaticamente!
+    def get_object(self, category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Category.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, category_slug, format=None):
+        category = self.get_object(category_slug)
+        serializer = CategorySerializer(category)
         return Response(serializer.data)
